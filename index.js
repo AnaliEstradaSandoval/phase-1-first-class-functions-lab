d24da0a84b3d2daf7e06401a4dd708c7f49d692d
Anali Estrada Sandoval <analiestrada44@gmail.com>
2023-04-09 22:23:14 -0700

    first class functions completed

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    return drivers.slice(0,2)
+}
+   
+const returnLastTwoDrivers = function(drivers){
+    drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    return drivers.slice(2,4)
+}
+
+   
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+function createFareMultiplier(){
+    return function (integer){
+       return integer * integer
+    }
+}
+
+
+function fareDoubler(createFareMultiplier){
+    const doubleIt = createFareMultiplier * 2;
+    return doubleIt
+}
+
+
+function fareTripler(createFareMultiplier){
+    const tripleIt = createFareMultiplier * 3;
+    return tripleIt
+}
+
+
+
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers){
+    return drivers, returnFirstTwoDrivers()
+}
